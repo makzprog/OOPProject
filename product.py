13f295cb5aa1e3c502ce0b073378a5255a6b652d
makzprog <makzprog@gmail.com>
2024-11-24 22:01:25 +0100

    added gitignore etc

---
+++ product.py
@@ -0,0 +1,37 @@
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
