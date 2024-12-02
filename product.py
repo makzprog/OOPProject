5bf38b547993bf3897853ae58fd94e35d32f2d98
makzprog <makzprog@gmail.com>
2024-12-03 00:12:45 +0100

    added gitignore etc

--- product.py
+++ product.py
@@ -432,6 +432,450 @@ print(type(item1_quant))
 
 
 
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
+
+item1 = "phone"
+item1_price = 100
+item1_quant = 5
+item_price_total = item1_price * item1_quant 
+
+print(type(item1))
+print(type(item1_price))
+print(type(item1_quant))
+
+
+
 item1 = "phone"
 item1_price = 100
 item1_quant = 5
